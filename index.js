f843b353ddebafdf6ed5f8ad2bd6192f446d908b
Mark Barr <mark@hyperingenuity.com>
2019-04-24 23:35:28 -0400

    uscript: index updates for exports and shim

--- index.js
+++ index.js
@@ -2,5 +2,22 @@
 'use strict';
 
 const Lexer = require('./lexer');
+const Parser = require('./parser');
 
-module.exports = { Lexer };
+Object.defineProperty(String.prototype, 'µ', {
+  value() {
+    return 'µscript';
+  },
+  enumerable: false,
+  configurable: true
+});
+
+const λ = function() {
+  return 'µscript';
+};
+
+module.exports = {
+  Lexer,
+  Parser,
+  λ
+};
