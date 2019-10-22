41cb2a91a80ab0c3959bb19842abf06ed1e01022
Mark Barr <mark@hyperingenuity.com>
2019-10-22 11:26:32 -0400

    uscript: minimal cli added

---
+++ cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+
+require('barrkeep/pp');
+const Uscript = require('./uscript');
+const uscript = new Uscript();
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'uscript> '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  try {
+    const result = uscript.eval(line);
+    console.pp(result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  rl.prompt();
+}).on('close', () => {
+  console.log('EOF');
+  process.exit(0);
+});
